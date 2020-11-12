4cd8c811cd2431612e191d541bb3680c1fca707b
Sid <sid.ghodke@gmail.com>
2020-11-12 07:29:50 -0800

    Import react-bootstrap
    Fixes Modal logic with Bootstrap 5

--- pages/cheer/index.js
+++ pages/cheer/index.js
@@ -5,61 +5,67 @@ import Footer from "../../components/footer";
 import BoardSummary from "../../components/board-summary";
 import CreateBoard from "../../components/create-board";
 
-// import "bootstrap/js/src/modal";
-// import dynamic from "next/dist/next-server/lib/dynamic";
-// const modal = dynamic(() => import("bootstrap/js/src/modal"))
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
 
 
 export default function ListBoards() {
     const [filter, setFilter] = useState('given');
     const [showModal, setModal] = useState(false);
+
+    const handleClose = () => setModal(false);
+    const handleShow = () => setModal(true);
+
     return (
         <>
             <Header/>
-            <div className="container">
+
+            <Container>
                 <div>
-                    <button type="button" className={`btn ${ filter === 'given' ? "btn-primary" : "btn-link"}`}
+                    <Button variant={filter === 'given' ? "primary" : "link"}
                             onClick={() => setFilter('given')}>
-                        Given <span className="badge bg-secondary">3</span>
-                    </button>
+                        Given
+                    </Button>
                     |
-                    <button type="button" className={`btn ${ filter === 'received' ? "btn-primary" : "btn-link"}`}
+                    <Button variant={filter === 'received' ? "primary" : "link"}
                             onClick={() => setFilter('received')}>
-                        Received <span className="badge bg-secondary">1</span>
-                    </button>
+                        Received
+                    </Button>
                     |
-                    <button type="button" className="btn btn-link"
-                            onClick={() => setModal(true)}  data-toggle="modal" data-target="#exampleModal">
+                    <Button
+                        onClick={() => setModal(true)}  data-toggle="modal" data-target="#exampleModal">
                         New Cheer!
-                    </button>
+                    </Button>
                 </div>
 
                 {[...Array( filter === 'given' ? 3 : 1).keys()].map(id =>
                     <BoardSummary id={id} key={id}/>
                 )}
-            </div>
+            </Container>
 
-            <div className={`modal fade ${showModal ? 'show' : ''}`} style={{display: showModal ? "block" : "none"}} tabIndex="-1" aria-labelledby="exampleModalLabel"
-                 aria-hidden="true">
-                <div className="modal-dialog modal-dialog-centered">
-                    <div className="modal-content">
-                        {/*<div className="modal-header">*/}
-                        {/*    <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>*/}
-                        {/*    <button type="button" className="btn-close" data-dismiss="modal"*/}
-                        {/*            onClick={() => setModal(false)}*/}
-                        {/*            aria-label="Close"></button>*/}
-                        {/*</div>*/}
-                        <div className="modal-body">
-                            <CreateBoard/>
-                        </div>
-                        <div className="modal-footer">
-                            <button type="button" className="btn btn-secondary" data-dismiss="modal"
-                                 onClick={() => setModal(false)}>Close</button>
-                            <button type="button" className="btn btn-primary">Save changes</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+
+            <Modal show={showModal} onHide={handleClose}
+                   size="lg"
+                   aria-labelledby="contained-modal-title-vcenter"
+                   centered
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <CreateBoard/>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Save Changes
+                    </Button>
+                </Modal.Footer>
+            </Modal>
 
             <Footer/>
         </>
