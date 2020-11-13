a19428140fe4dfbbd153d788f080b762b96b8aac
Sid <sid.ghodke@gmail.com>
2020-11-12 20:40:51 -0800

    Stylize Editor

--- pages/cheer/[id]/index.js
+++ pages/cheer/[id]/index.js
@@ -6,6 +6,7 @@ import styles from "../../../styles/Editor.module.css"
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+import Footer from "../../../components/footer";
 
 /*
 References
@@ -30,7 +31,7 @@ export default class Editor extends React.Component {
 
         this.columnGrids = this.itemContainers.map((container) => {
             return new Muuri(container.current, {
-                items: '.board-item',
+                items: '.' + styles.boardItem,
                 layoutDuration: 400,
                 layoutEasing: 'ease',
                 dragReleaseDuration: 400,
@@ -82,25 +83,18 @@ export default class Editor extends React.Component {
                     <Row>
                         <Col className={styles.boardColumn}>
                             <div className={styles.boardColumnContent} ref={this.itemContainers[0]}>
-                                <div className={styles.boardItem}>
-                                    <div className={styles.boardItemContent}><span>Item #</span>1</div>
-                                </div>
-                                <div className={styles.boardItem}>
-                                    <div className={styles.boardItemContent}><span>Item #</span>2</div>
-                                </div>
                                 <Pin id={1}/>
                                 <Pin id={2}/>
-
                             </div>
                         </Col>
-                        <Col className="board-column">
-                            <div className="board-column-content" ref={this.itemContainers[1]}>
+                        <Col className={styles.boardColumn}>
+                            <div className={styles.boardColumnContent} ref={this.itemContainers[1]}>
                                 <Pin id={3}/>
                                 <Pin id={4}/>
                             </div>
                         </Col>
-                        <Col className="board-column">
-                            <div className="board-column-content" ref={this.itemContainers[2]}>
+                        <Col className={styles.boardColumn}>
+                            <div className={styles.boardColumnContent} ref={this.itemContainers[2]}>
                                 <Pin id={5}/>
                                 <div className={styles.boardItem}>
                                     <div className={styles.boardItemContent}><span>Item #</span>11</div>
@@ -112,11 +106,24 @@ export default class Editor extends React.Component {
                         </Col>
                     </Row>
                 </Container>
-                <style global jsx>
-                    {`
-
-`}
-                </style>
+                <style jsx global>{`
+                    .muuri-item-placeholder {
+                        z-index: 9;
+                        margin: 0;
+                        opacity: 0.7;
+                    }
+                    .muuri-item-releasing {
+                        z-index: 9998;
+                    }
+                    .muuri-item-dragging {
+                        z-index: 19999;
+                        cursor: move;
+                    }
+                    .muuri-item-hidden {
+                        z-index: 0;
+                    }
+                `}</style>
+                <Footer/>
             </>
         );
     }
