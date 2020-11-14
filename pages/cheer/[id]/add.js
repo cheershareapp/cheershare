466cd6b7c7778275b47bdf1b4838668c7ce49a09
Sid <sid.ghodke@gmail.com>
2020-11-13 18:37:39 -0800

    Add results area for the search page

--- pages/cheer/[id]/add.js
+++ pages/cheer/[id]/add.js
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import React, {useState} from "react";
 import {signIn, signOut, useSession} from "next-auth/client";
 
 import Header from "../../../components/header";
@@ -9,8 +9,12 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Nav from "react-bootstrap/Nav";
 import InputGroup from 'react-bootstrap/InputGroup'
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
+import Alert from "react-bootstrap/Alert";
+import {useRouter} from "next/router";
 
-const ImagePage = ({ onSelect }) => <Nav variant="tabs" defaultActiveKey="" onSelect={onSelect}>
+const ImagePage = ({ onSelect }) => <Nav defaultActiveKey="" onSelect={onSelect}>
     <Nav.Item>
         <Nav.Link eventKey="upload-image">Upload Image</Nav.Link>
     </Nav.Item>
@@ -22,7 +26,7 @@ const ImagePage = ({ onSelect }) => <Nav variant="tabs" defaultActiveKey="" onSe
     </Nav.Item>
 </Nav>;
 
-const VideoPage = ({ onSelect }) => <Nav variant="tabs" defaultActiveKey="" onSelect={onSelect}>
+const VideoPage = ({ onSelect }) => <Nav defaultActiveKey="" onSelect={onSelect}>
     <Nav.Item>
         <Nav.Link eventKey="upload-video">Upload Video</Nav.Link>
     </Nav.Item>
@@ -31,9 +35,9 @@ const VideoPage = ({ onSelect }) => <Nav variant="tabs" defaultActiveKey="" onSe
     </Nav.Item>
 </Nav>;
 
-const SearchPage = ({ vendor }) => <Form>
-    <Form.Group controlId="formBasicEmail">
-        <InputGroup className="mb-3">
+const SearchPage = ({ vendor }) => <><Form>
+    <Form.Group controlId="formSearch">
+        <InputGroup>
             <Form.Control
                 placeholder="Search for..."
                 aria-label="Search for..."
@@ -44,24 +48,38 @@ const SearchPage = ({ vendor }) => <Form>
             </InputGroup.Append>
         </InputGroup>
 
-        <Form.Text className="text-muted">
+        <Form.Text className="text-muted text-right">
             Powered by {vendor}
         </Form.Text>
     </Form.Group>
 </Form>
-
-const UploadPage = ({ filetype }) => <>
-    Add an {filetype} from your computer or phone
-    <Form.File
-        label={`Drop your ${filetype} file here`}
-        custom
-    />
+    <Alert className="overflow-auto text-center" style={{maxHeight: "30vh"}} variant="info">
+        {[...Array(10)].map(() =>
+        <svg className="bd-placeholder-img m-3" width="200" height="200"
+             xmlns="http://www.w3.org/2000/svg" role="img"
+             aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice"
+             focusable="false"><title>Placeholder</title>
+            <rect width="100%" height="100%" fill="#55595c"/>
+            <text x="30%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+        </svg>)}
+    </Alert>
 </>
 
+const UploadPage = ({ filetype }) => <Form className="min-vh-20">
+    Add an {filetype} from your computer or phone
+    <Alert style={{minHeight: "15vh"}} className="text-center" variant="success">
+        <h5>Drop your {filetype} file here</h5>
+        <hr />
+        <Form.File id="formcheck-api-custom" custom className="my-4">
+            <Form.File.Label data-browse="Button text">
+            </Form.File.Label>
+            <Form.Control.Feedback type="valid">You did it!</Form.Control.Feedback>
+        </Form.File>
+    </Alert>
+</Form>
+
 function renderPage(page, setPage) {
-    switch(page) {
-        case "image": return <ImagePage onSelect={setPage}/>;
-        case "video": return <VideoPage onSelect={setPage}/>;
+    switch (page) {
         case "unsplash": return <SearchPage vendor="unsplash"/>;
         case "giphy": return <SearchPage vendor="giphy"/>;
         case "youtube": return <SearchPage vendor="youtube"/>;
@@ -76,6 +94,7 @@ function renderPage(page, setPage) {
 
 export default function Add() {
     const [ page, setPage ] = useState('');
+    const router = useRouter()
 
     return (<>
         <Header/>
@@ -84,14 +103,14 @@ export default function Add() {
             <div className="py-5">
                 <h1>Add a Post</h1>
             </div>
-            <Nav defaultActiveKey="" as="ul" onSelect={setPage}>
-                <Nav.Item as="li">
-                    <Nav.Link eventKey="image">Add Image</Nav.Link>
-                </Nav.Item>
-                <Nav.Item as="li">
-                    <Nav.Link eventKey="video">Add Video</Nav.Link>
-                </Nav.Item>
-            </Nav>
+            <Tabs defaultActiveKey="" variant="pills" onSelect={setPage}>
+                <Tab eventKey="image" title="Add Image">
+                    <ImagePage onSelect={setPage}/>
+                </Tab>
+                <Tab eventKey="video" title="Add Video">
+                    <VideoPage onSelect={setPage}/>
+                </Tab>
+            </Tabs>
             { renderPage(page, setPage) }
             <Form>
                 <Form.Control as="textarea" rows={10} />
@@ -99,7 +118,7 @@ export default function Add() {
                 <Button variant="primary" type="submit">
                     Post
                 </Button>
-                <Button variant="link">
+                <Button variant="link" onClick={() => router.back()}>
                     Discard Post
                 </Button>
             </Form>
