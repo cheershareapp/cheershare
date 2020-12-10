e153d7545f302cb151f16cb63238395f2bbe1bbe
Sid <sid.ghodke@gmail.com>
2020-12-10 06:48:54 -0800

    Convert Pin into a functional component

--- components/pin.js
+++ components/pin.js
@@ -3,31 +3,22 @@ import {Card, Button, ButtonGroup} from "react-bootstrap";
 import styles from "../styles/Editor.module.css"
 import Image from 'next/image';
 
-export default class CheerPin extends React.Component {
-    componentDidMount() {
-    }
+export default function CheerPin ({mediaUrl, id, previewMode, ownerName, message}) {
+    return (<div className={styles.boardItem} data-id={id}>
+        <Card className="shadow-sm">
+            { mediaUrl &&
+                <Image src={mediaUrl} className="card-img bd-placeholder-img card-img-top" width="100%" height="100%" layout="responsive"/>}
+            <Card.Body className={styles.boardItemContent}>
+                <Card.Text> {message} </Card.Text>
+                <div className="d-flex justify-content-between align-items-center">
+                    {!previewMode && <ButtonGroup>
+                        <Button variant="outline-secondary">Like</Button>
+                        <Button variant="outline-secondary">Edit</Button>
+                    </ButtonGroup>}
 
-    render() {
-        const imageSource = this.props.mediaUrl;
-
-        return (
-            <div className={styles.boardItem} data-id={this.props.id}>
-            <Card className="shadow-sm">
-                {/*<Card.Img src={imageSource} className="bd-placeholder-img card-img-top" width="100%"/>*/}
-                { imageSource &&
-                    <Image src={imageSource} className="card-img bd-placeholder-img card-img-top" width="100%" height="100%" layout="responsive"/>}
-                <Card.Body className={styles.boardItemContent}>
-                    <Card.Text> {this.props.message} </Card.Text>
-                    <div className="d-flex justify-content-between align-items-center">
-                        {!this.props.previewMode && <ButtonGroup>
-                            <Button variant="outline-secondary">Like</Button>
-                            <Button variant="outline-secondary">Edit</Button>
-                        </ButtonGroup>}
-                        <small className="text-muted">{this.props.ownerName}</small>
-                    </div>
-                </Card.Body>
-            </Card>
-            </div>
-        );
-    }
+                    <small className="text-muted">{ownerName}</small>
+                </div>
+            </Card.Body>
+        </Card>
+    </div>);
 }
