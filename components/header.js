68385b4f00cb72a451668c4608e6065eab24ea3c
Sid <sid.ghodke@gmail.com>
2020-12-11 15:37:24 -0800

    Add profile image to the header

--- components/header.js
+++ components/header.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import {signIn, signOut, useSession} from "next-auth/client";
-import {Container, Navbar, Nav, NavDropdown, Dropdown, DropdownButton, Button} from "react-bootstrap";
+import {Container, Navbar, Nav, Dropdown, DropdownButton, Button, Image} from "react-bootstrap";
 import CreateBoardModal from "./create-board";
 import Link from "next/link";
 
@@ -26,14 +26,17 @@ export default function Header(props) {
                             <Nav.Link href="/cheer" as={Link}>Dashboard</Nav.Link>
                             <Nav.Link onClick={() => setModal(true)}>New Board</Nav.Link>
 
-                            <svg width="2em" height="2em" viewBox="0 0 16 16" className="bi bi-person-circle"
-                                 fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="M13.468 12.37C12.758 11.226 11.195 10 8 10s-4.757 1.225-5.468 2.37A6.987 6.987 0 0 0 8 15a6.987 6.987 0 0 0 5.468-2.63z"/>
-                                <path fillRule="evenodd" d="M8 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
-                                <path fillRule="evenodd"
-                                      d="M8 1a7 7 0 1 0 0 14A7 7 0 0 0 8 1zM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8z"/>
-                            </svg>
+                            { 'image' in session.user ?
+                                <Image src={session.user.image} roundedCircle style={{width:"2.5em", height:"2.5em"}}/> :
+                                (<svg width="2em" height="2em" viewBox="0 0 16 16" className="bi bi-person-circle"
+                                      fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M13.468 12.37C12.758 11.226 11.195 10 8 10s-4.757 1.225-5.468 2.37A6.987 6.987 0 0 0 8 15a6.987 6.987 0 0 0 5.468-2.63z"/>
+                                    <path fillRule="evenodd" d="M8 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                                    <path fillRule="evenodd"
+                                          d="M8 1a7 7 0 1 0 0 14A7 7 0 0 0 8 1zM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8z"/>
+                                </svg>)
+                            }
                             <DropdownButton
                                 variant="link"
                                 menuAlign="right"
@@ -41,7 +44,7 @@ export default function Header(props) {
                                 title=""
                             >
                                 <Dropdown.Header>{session.user.email || 'Email'}</Dropdown.Header>
-                                <Dropdown.Item><Link href="/profile/">Account</Link></Dropdown.Item>
+                                <Dropdown.Item href="/profile/"><Link href="/profile/">Account</Link></Dropdown.Item>
                                 <Dropdown.Item eventKey="2">History</Dropdown.Item>
                                 <Dropdown.Item eventKey="3">Invite Friend</Dropdown.Item>
                                 <Dropdown.Divider />
