d6b504eef738e89d1de78b8bac95e31127a1dfd1
Sid <sid.ghodke@gmail.com>
2020-12-11 16:10:08 -0800

    Add padding for the Card

--- components/CheerPin/index.js
+++ components/CheerPin/index.js
@@ -7,13 +7,13 @@ import {Draggable} from "react-beautiful-dnd";
 export default function CheerPin ({mediaUrl, id, editable, ownerName, message, index}) {
     return (<Draggable draggableId={id} index={index}>
         {(provided, snapshot) => (
-        <Card className="shadow-sm"
+        <Card className="shadow-sm my-2"
               ref={provided.innerRef}
               {...provided.draggableProps}
               {...provided.dragHandleProps}
         >
             { mediaUrl &&
-                <Image src={mediaUrl} className="card-img bd-placeholder-img card-img-top" width="100%" height="100%" layout="responsive"/>}
+            <Image src={mediaUrl} className="card-img bd-placeholder-img card-img-top" width="100%" height="100%" layout="responsive"/>}
             <Card.Body className={styles.boardItemContent}>
                 <Card.Text> {message} </Card.Text>
                 <div className="d-flex justify-content-between align-items-center">
